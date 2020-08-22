94ffe479bd2d9062ac4f7aa55890f2047d44f2bf
Mayank <mayank@Alishas-MacBook-Air-2.local>
2020-08-22 18:26:52 +0530

    Updates

---
+++ precache-manifest.5f4ac466d77e6975fd8c8d7239571443.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57625b77be19ef4583625b72240526b6",
+    "url": "/workflow-system/index.html"
+  },
+  {
+    "revision": "7052a893645b248747f0",
+    "url": "/workflow-system/static/css/main.44e2d3b3.chunk.css"
+  },
+  {
+    "revision": "d07c37517c9f868a2932",
+    "url": "/workflow-system/static/js/2.e9721cb3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/workflow-system/static/js/2.e9721cb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7052a893645b248747f0",
+    "url": "/workflow-system/static/js/main.b6198d6b.chunk.js"
+  },
+  {
+    "revision": "86292d4e6a273e53af25",
+    "url": "/workflow-system/static/js/runtime-main.66cbfda1.js"
+  }
+]);
