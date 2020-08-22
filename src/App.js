2530cc8af347aa7133aab17810cf9bc8e26e0f7d
mayank8aug <mayank@Alishas-MacBook-Air-2.local>
2020-08-22 09:12:52 +0530

    React-Redux Initial Setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+  
+import React from 'react';
+import './Common.css';
+
+const App = () => {
+  return (
+    <div className="app-container">
+      Hello
+    </div>
+  )
+}
+
+export default App;
