e69cbfa1539c7626908935322b5a9851b96705ef
mayank8aug <mayank@Alishas-MacBook-Air-2.local>
2020-08-24 09:29:13 +0530

    Added back button on workflow edit page

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,6 +1,7 @@
 import React, { useCallback } from 'react';
 import { useHistory } from "react-router-dom";
 import { useSelector, useDispatch } from 'react-redux';
+import { FaArrowCircleLeft } from 'react-icons/fa';
 import { logoutUser } from '../../actions/index';
 import './Header.css'
 
@@ -14,9 +15,13 @@ function Header() {
     const dispatch = useDispatch();
     const logout = useCallback(userLogout.bind(null, dispatch, history));
     const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
+    const isBack = history.location.pathname.indexOf('workflows/') > -1;
     return (
         <div className="header">
-            <div className="fs20 fw700 color-white">FLOWAPP</div>
+            <div className="fs20 fw700 color-white display-flex align-items-center">
+                {isBack && <span className="back-btn" onClick={history.goBack}><FaArrowCircleLeft /></span>}
+                <span className="pd-l4">WORKFLOW SYSTEM</span>
+            </div>
             {isLoggedIn && <button className="logout-btn" onClick={logout}>Logout</button>}
         </div>
     );
